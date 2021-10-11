2c941859d36530a36f6ec9675702528891fbee72
Vilas Patil <patildipak422@gmail.com>
2021-10-11 22:43:09 +0530

    added steps

--- nopCommerce/src/test/java/stepDefinitions/Steps.java
+++ nopCommerce/src/test/java/stepDefinitions/Steps.java
@@ -1,5 +1,43 @@
 package stepDefinitions;
 
-public class Steps {
+import cucumber.api.java.en.*;
 
+public class Steps {
+	
+	
+	
+	@Given("^I launch Chrome Browser$")
+	public void i_launch_Chrome_Browser() throws Exception {
+	    
+	}
+
+	@When("^I open URL \"([^\"]*)\"$")
+	public void i_open_URL(String arg1) throws Exception {
+	    
+	}
+
+	@When("^I enter Email as \"([^\"]*)\" and password as \"([^\"]*)\"$")
+	public void i_enter_Email_as_and_password_as(String arg1, String arg2) throws Exception {
+	    
+	}
+
+	@When("^I click login$")
+	public void i_click_login() throws Exception {
+	    
+	}
+
+	@Then("^Page title should be \"([^\"]*)\"$")
+	public void page_title_should_be(String arg1) throws Exception {
+	    
+	}
+
+	@When("^I click logout$")
+	public void i_click_logout() throws Exception {
+	    
+	}
+
+	@Then("^I close Browser$")
+	public void i_close_Browser() throws Exception {
+	    
+	}
 }
