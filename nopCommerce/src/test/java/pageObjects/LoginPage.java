9d5042c53a4bf92b99e02d51e90f2ea3427a896d
Vilas Patil <patildipak422@gmail.com>
2021-10-19 23:01:41 +0530

    added first scenario

--- nopCommerce/src/test/java/pageObjects/LoginPage.java
+++ nopCommerce/src/test/java/pageObjects/LoginPage.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
-	@FindBy(xpath = "//input[@id='Email']")public WebElement email;
-	@FindBy(xpath = "//input[@id='Password']")public WebElement password;
-	@FindBy(xpath = "//button[contains(text(),'Log in')]")public WebElement login;
-	@FindBy(xpath = "//a[contains(text(),'Logout')]")public WebElement logout;
+	@FindBy(xpath = "//input[@id='Email']")public static WebElement email;
+	@FindBy(xpath = "//input[@id='Password']")public static WebElement password;
+	@FindBy(xpath = "//button[contains(text(),'Log in')]")public static WebElement login;
+	@FindBy(xpath = "//a[contains(text(),'Logout')]")public static WebElement logout;
 	
 }
--- nopCommerce/src/test/java/stepDefinitions/Steps.java
+++ nopCommerce/src/test/java/stepDefinitions/Steps.java
@@ -1,43 +1,62 @@
 package stepDefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.en.*;
+import pageObjects.LoginPage;
 
 public class Steps {
 	
-	
+	protected static WebDriver driver;
 	
 	@Given("^I launch Chrome Browser$")
 	public void i_launch_Chrome_Browser() throws Exception {
-	    
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\patil\\Desktop\\Programs\\Projects\\nopCommerce\\Drivers\\chromedriver.exe");  
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
 	}
 
 	@When("^I open URL \"([^\"]*)\"$")
-	public void i_open_URL(String arg1) throws Exception {
-	    
+	public void i_open_URL(String url) throws Exception {
+		driver.get(url);
+		Thread.sleep(3000);
 	}
 
 	@When("^I enter Email as \"([^\"]*)\" and password as \"([^\"]*)\"$")
-	public void i_enter_Email_as_and_password_as(String arg1, String arg2) throws Exception {
-	    
+	public void i_enter_Email_as_and_password_as(String Email, String password) throws Exception {
+		LoginPage.email.clear();
+		Thread.sleep(2000);
+		LoginPage.email.sendKeys(Email);
+		Thread.sleep(2000);
+		LoginPage.password.clear();
+		Thread.sleep(2000);
+		LoginPage.password.sendKeys(password);
+		Thread.sleep(2000);
 	}
 
 	@When("^I click login$")
 	public void i_click_login() throws Exception {
-	    
+	    LoginPage.login.click();
 	}
 
 	@Then("^Page title should be \"([^\"]*)\"$")
-	public void page_title_should_be(String arg1) throws Exception {
-	    
+	public void page_title_should_be(String title) throws Exception {
+	    if(driver.getTitle().equalsIgnoreCase(title)==true) {
+	    	System.out.println("Title is verified");
+	    }else {
+	    	System.out.println("Title is wrong");
+	    	driver.close();
+	    }
 	}
 
 	@When("^I click logout$")
 	public void i_click_logout() throws Exception {
-	    
+	    LoginPage.logout.click();
 	}
 
 	@Then("^I close Browser$")
 	public void i_close_Browser() throws Exception {
-	    
+	    driver.close();
 	}
 }
