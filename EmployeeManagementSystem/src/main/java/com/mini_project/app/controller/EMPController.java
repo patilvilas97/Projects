b6c251cad2e6f017096e92e148212b13a25c7ff5
Vilas Patil <patildipak422@gmail.com>
2022-11-01 09:03:17 +0530

    Added the homepage  - yet to complete

--- EmployeeManagementSystem/src/main/java/com/mini_project/app/controller/EMPController.java
+++ EmployeeManagementSystem/src/main/java/com/mini_project/app/controller/EMPController.java
@@ -1,12 +1,14 @@
 package com.mini_project.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class EMPController {
 
 	@GetMapping(value="/")
 	public String home() {
-		return "index";
+		return "homepage";
 	}
 	
 }
