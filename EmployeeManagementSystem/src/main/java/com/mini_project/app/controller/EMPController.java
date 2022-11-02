3353de3d5217f403fe33684241af9e47af245f18
Vilas Patil <patildipak422@gmail.com>
2022-11-02 21:52:08 +0530

    Added Index.html

--- EmployeeManagementSystem/src/main/java/com/mini_project/app/controller/EMPController.java
+++ EmployeeManagementSystem/src/main/java/com/mini_project/app/controller/EMPController.java
@@ -2,6 +2,7 @@ package com.mini_project.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class EMPController {
