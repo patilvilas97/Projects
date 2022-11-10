606f6e8265382cd3f6c25d8f649e040ec77b52a0
Vilas Patil <patildipak422@gmail.com>
2022-11-10 22:38:39 +0530

    m

--- EmployeeManagementSystem/src/main/java/com/mini_project/app/EmployeeManagementSystemApplication.java
+++ EmployeeManagementSystem/src/main/java/com/mini_project/app/EmployeeManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+		//try
 	}
 
 }
--- EmployeeManagementSystem/src/main/java/com/mini_project/app/controller/EMPController.java
+++ EmployeeManagementSystem/src/main/java/com/mini_project/app/controller/EMPController.java
@@ -1,13 +1,13 @@
 package com.mini_project.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class EMPController {
 
-	@GetMapping(value="/")
+	@RequestMapping(value="/")
 	public String home() {
 		return "homepage";
 	}
