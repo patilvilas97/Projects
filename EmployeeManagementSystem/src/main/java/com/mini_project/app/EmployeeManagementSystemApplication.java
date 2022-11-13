be516d1f869b8e2152316d4ebff52a0b4456b35f
Vilas Patil <patildipak422@gmail.com>
2022-11-13 22:28:04 +0530

    n

--- EmployeeManagementSystem/src/main/java/com/mini_project/app/EmployeeManagementSystemApplication.java
+++ EmployeeManagementSystem/src/main/java/com/mini_project/app/EmployeeManagementSystemApplication.java
@@ -8,7 +8,7 @@ public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
-		//try
+		
 	}
 
 }
