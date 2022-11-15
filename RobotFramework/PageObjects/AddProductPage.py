c696261f64bae7bb908718a71d994777c692657c
Vilas Patil <patildipak422@gmail.com>
2022-11-15 23:36:20 +0530

    Managing Directory

--- RobotFramework/PageObjects/AddProductPage.py
+++
@@ -1,4 +0,0 @@
-# Add Product Element
-
-Catalog = "xpath:(//a[@class='nav-link'])[4]"
-product = "xpath:(//p[contains(text(),' Products')])[1]"
--- RobotFramework/PageObjects/LoginPage.py
+++
@@ -1,6 +0,0 @@
-#Login Page Elements
-
-# login_Link = "xpath://a[contains(text(),'Log in')]"
-login_UserName = "xpath://input[@id='Email']"
-login_Password = "xpath://input[@id='Password']"
-login_Button = "xpath://button[contains(text(),'Log in')]"
