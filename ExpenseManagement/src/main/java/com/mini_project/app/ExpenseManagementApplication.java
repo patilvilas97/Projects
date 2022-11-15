645370bbb78a70dc57187a799e8249f7ea1fce74
Vilas Patil <patildipak422@gmail.com>
2022-11-15 22:49:59 +0530

    Initial commit

---
+++ ExpenseManagement/src/main/java/com/mini_project/app/ExpenseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mini_project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementApplication.class, args);
+	}
+
+}
